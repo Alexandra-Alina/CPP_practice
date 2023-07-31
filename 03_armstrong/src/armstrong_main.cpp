f18024f44e652401fd2ceb2ff67c5c4e0fcce9da
Alina <alina.dumitrica22@gmail.com>
2023-07-31 14:30:28 +0300

    Tema1

--- 03_armstrong/src/armstrong_main.cpp
+++ 03_armstrong/src/armstrong_main.cpp
@@ -1,10 +1,42 @@
 #include <iostream>
+#include<math.h>
+#include<string>
+
+using namespace std;
+
+//vedem cate cifre are - determinam puterea cifrelor din suma
+int putere(int number)
+{
+	int putere_numar = 0;
+	while (number > 0)
+	{
+		number /= 10;
+		putere_numar++;
+	}
+	return putere_numar;
+}
+
+
+// calculam suma de puteri
+int sumaCifre(int number)
+{
+	int putere_numar = putere(number);
+	int sum = 0;
+	while (number > 0)
+	{
+		int cifra = number % 10;
+		sum = sum + pow(cifra, putere_numar);
+		number /= 10;
+	}
+	return sum;
+}
 
 bool isArmstrongNumber(int number)
 {
-	// TODO: implement some functionality to see if this number is an armstrong number
+	int suma_cifre = sumaCifre(number);
 
-	return false;
+	return number == suma_cifre;
+
 }
 
 void printIsArmstrong(int number)
@@ -21,38 +53,36 @@ void printIsArmstrong(int number)
 
 int main(int argc, char *argv[])
 {
-	// What is this program expected to do?
-	// - Shows whether an argument is an armstrong number.
-	// (what?)
-	// -	An Armstrong number is a number that is equal to the sum of cubes of its digits.
-	//		For example 0, 1, 153, 370, 371 and 407 are the Armstrong numbers.
-	//		Let's try to understand why 153 is an Armstrong number:
-	//			1^3 + 5^3 + 3^3 = 1 + 125 + 27 = 153
-	//
-	// How to launch it?
-	// - Execute the binary and pass a parameter to it?
-	// - E.g. Open CMD in bin/Debug or bin/Release
-	//		  03_armstrong.exe 1		=> Output: Armstrong
-	//		  03_armstrong.exe 2		=> Output: NOT Armstrong
-	//		  03_armstrong.exe 			=> Output: No program arguments found.
-	//		  03_armstrong.exe ABC		=> Undefined output (do whatever).
-	//		  03_armstrong.exe 153		=> Output: Armstrong
-	//		  03_armstrong.exe 154		=> Output: NOT Armstrong
-	//
-
 	// Make sure there are some program arguments available.
 	if (argc <= 1)
 	{
-		std::cout << "No program arguments found." << std::endl;
+		cout << "No program arguments found." << std::endl;
 		return 1;
 	}
 
-	int readNumber = 0;
 	// Get the first argument
-	std::string argumentAsString = argv[1];
-	
+	string argumentAsString = argv[1];
+
 	// TODO: read number / cast to integer
 
-	printIsArmstrong(readNumber);
+	int number;
+
+	try
+	{
+		string::size_type sz;
+		number = stoi(argumentAsString, &sz);
+
+		if (!argumentAsString.substr(sz).empty())
+		{
+			throw invalid_argument("Undefined output");
+		}
+	}
+	catch (const invalid_argument ia)
+	{
+		cout << ia.what() << endl;
+		return 1;
+	}
+
+	printIsArmstrong(number);
 	return 0;
 }
